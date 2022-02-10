82f2639751edab4913543990286622733b4a9e64
Fabricio <fabriciogiac@gmail.com>
2022-02-10 10:31:57 -0300

    Remove python prototype

--- src/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from assembler import Assembler
-
-
-if __name__ == '__main__':
-    input = ' '.join(sys.argv[1:]).replace('\\n', '\n')
-    data = Assembler().assemble_prettyhex(input)
-    print(data)
